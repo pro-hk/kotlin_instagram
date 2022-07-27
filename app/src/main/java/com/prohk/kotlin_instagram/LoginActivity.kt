ea21fc9c94ef66457bb1218b2218c75c686e93c4
pro-hk <pro_hk@naver.com>
2022-07-27 17:45:50 +0900

    구글로그인 추가

--- app/src/main/java/com/prohk/kotlin_instagram/LoginActivity.kt
+++ app/src/main/java/com/prohk/kotlin_instagram/LoginActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.prohk.kotlin_instagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -14,6 +20,10 @@ class LoginActivity : AppCompatActivity() {
 
     // Authentication 라이브러리 생성
     var auth: FirebaseAuth? = null
+
+    // google - SHA 인증서 해야됨
+    var googleSignInClient: GoogleSignInClient? = null
+    val GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +35,51 @@ class LoginActivity : AppCompatActivity() {
         binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
         }
+
+        // google 옵션
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googleSignInButton.setOnClickListener {
+            // First step
+            googleLogin()
+        }
+    }
+
+    // 구글 로그인
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
+            if(result!!.isSuccess) {
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credentail = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credentail)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    // 아이디와 패스워드가 맞았을 때
+                    moveMainPage(task.result.user) // 메인페이지 이동
+                } else {
+                    // 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show() // 에러메시지 호출
+                }
+            }
     }
 
     // 아이디 생성
@@ -48,7 +103,7 @@ class LoginActivity : AppCompatActivity() {
 
     // 로그인
     fun singinEmail() {
-        auth?.createUserWithEmailAndPassword(
+        auth?.signInWithEmailAndPassword(
             binding.emailEdittext.text.toString(),
             binding.passwordEdittext.text.toString()
         )?.addOnCompleteListener { task ->
