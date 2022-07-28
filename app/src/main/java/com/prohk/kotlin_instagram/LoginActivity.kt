533b2050d7580084801df79832cec2e539929a3e
pro-hk <pro_hk@naver.com>
2022-07-28 18:11:48 +0900

    페이스북 로그인 추가

--- app/src/main/java/com/prohk/kotlin_instagram/LoginActivity.kt
+++ app/src/main/java/com/prohk/kotlin_instagram/LoginActivity.kt
@@ -1,18 +1,30 @@
 package com.prohk.kotlin_instagram
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import com.prohk.kotlin_instagram.databinding.ActivityLoginBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -24,6 +36,9 @@ class LoginActivity : AppCompatActivity() {
     // google - SHA 인증서 해야됨
     var googleSignInClient: GoogleSignInClient? = null
     val GOOGLE_LOGIN_CODE = 9001
+
+    // facebook - callbackmanager
+    var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +63,71 @@ class LoginActivity : AppCompatActivity() {
             // First step
             googleLogin()
         }
+
+        // 페이스북 해시태그
+        //printHashKey()
+
+        // 페이스북
+        callbackManager = CallbackManager.Factory.create()
+
+        binding.facebookLoginButton.setOnClickListener {
+            // First step
+            facebookLogin()
+        }
+    }
+
+    // 페이스북 해시키
+    fun printHashKey() {
+        try {
+            val info: PackageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey: String = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
+    }
+
+    // 페이스북 로그인
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException) {
+
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    // Second step
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+            })
+    }
+
+    fun handleFacebookAccessToken(token: AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    // Third step
+                    // 아이디와 패스워드가 맞았을 때
+                    moveMainPage(task.result.user) // 메인페이지 이동
+                } else {
+                    // 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show() // 에러메시지 호출
+                }
+            }
     }
 
     // 구글 로그인
@@ -56,8 +136,14 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
+    // 액티비티 결과 값 수신
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        // 페이스북 콜백
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+
+        // 구글 로그인 결과
         if(requestCode == GOOGLE_LOGIN_CODE) {
             var result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
             if(result!!.isSuccess) {
@@ -91,7 +177,7 @@ class LoginActivity : AppCompatActivity() {
             if(task.isSuccessful) {
                 // 아이디가 생성되었을 때 필요한 코드를 입력
                 moveMainPage(task.result.user) // 메인페이지 이동
-            } else if(!task.exception?.message.isNullOrBlank()) {
+            } else if(!task.exception?.message.isNullOrEmpty()) {
                 // 로그인 에러가 나타났을 때 에러 메시지 출력
                 Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show() // 에러메시지 호출
             } else {
@@ -111,6 +197,7 @@ class LoginActivity : AppCompatActivity() {
                 // 아이디와 패스워드가 맞았을 때
                 moveMainPage(task.result.user) // 메인페이지 이동
             } else {
+                Toast.makeText(this,"로그인",Toast.LENGTH_LONG).show()
                 // 아이디와 패스워드가 틀렸을 때
                 Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show() // 에러메시지 호출
             }
